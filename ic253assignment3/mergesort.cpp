5c9fd684552c1aa134d46e9b28243be169961969
taneshqGupta <f.taneshq@gmail.com>
2025-07-30 09:04:01 +0530

    update

--- ic253assignment3/mergesort.cpp
+++ ic253assignment3/mergesort.cpp
@@ -3,41 +3,41 @@ using namespace std;
 
 int comparisons = 0, swaps = 0;
 
-void merge(int arr[], int l, int m, int r) {
+void merge(int a[], int l, int m, int r) {
     int n1 = m - l + 1, n2 = r - m;
     int L[n1], R[n2];
 
-    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
-    for (int i = 0; i < n2; i++) R[i] = arr[m + 1 + i];
+    for (int i = 0; i < n1; i++) L[i] = a[l + i];
+    for (int i = 0; i < n2; i++) R[i] = a[m + 1 + i];
 
     int i = 0, j = 0, k = l;
     while (i < n1 && j < n2) {
         comparisons++;
-        if (L[i] <= R[j]) arr[k++] = L[i++];
-        else arr[k++] = R[j++], swaps++;
+        if (L[i] <= R[j]) a[k++] = L[i++];
+        else a[k++] = R[j++], swaps++;
     }
 
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
+    while (i < n1) a[k++] = L[i++];
+    while (j < n2) a[k++] = R[j++];
 }
 
-void mergeSort(int arr[], int l, int r) {
+void mergeSort(int a[], int l, int r) {
     if (l >= r) return;
     int m = l + (r - l) / 2;
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-    merge(arr, l, m, r);
+    mergeSort(a, l, m);
+    mergeSort(a, m + 1, r);
+    merge(a, l, m, r);
 }
 
 int main() {
     int n;
     cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
 
-    mergeSort(arr, 0, n - 1);
+    mergeSort(a, 0, n - 1);
 
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
     cout << "\nComparisons: " << comparisons << "\nSwaps: " << swaps << "\n";
 }
 
